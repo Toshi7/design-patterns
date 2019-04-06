c647993c8fd79035707fb7c83db2519b77c87872
Takatoshi Tomoyose <ttoshi0719@gmail.com>
2019-04-07 08:32:28 +0900

    add state pattern

---
+++ Statepattern1/Statepattern1/Drying.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statepattern1
+{
+    class Drying : IState
+    {
+        public void DoAction(WashingmMachine washingMachine)
+        {
+            Console.WriteLine("乾燥しました。");
+            washingMachine.SetState(null);
+        }
+    }
+}
---
+++ Statepattern1/Statepattern1/IState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statepattern1
+{
+    interface IState
+    {
+        void DoAction(WashingmMachine washingMachine);
+    }
+}
